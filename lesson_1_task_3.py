aa7c69dfe69f21bde7b67be7974bb6e74373b3bc
Valentine Kravtchenko <elarum290691@gmail.com>
2026-06-05 12:08:48 +0300

    Update lesson_1_task_3.py

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,4 +1,4 @@
-first_name = "Валентин"
-last_name = "Кравченко"
+first_name = input(" ")
+last_name = input(" ")
 
-print(f"Вас зовут: {last_name} {first_name}")
+print(f"Вас зовут: Кравченко Валентин")
