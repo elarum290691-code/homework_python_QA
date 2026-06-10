cc91b8dd7c8dcc618b1ce7e6fb119eef28fd0a9b
Valentine Kravtchenko <elarum290691@gmail.com>
2026-06-10 09:41:18 +0300

    Update lesson_2_task_3.py

--- lesson_2_task_3.py
+++ lesson_2_task_3.py
@@ -3,10 +3,12 @@ import math
 
 def square(side):
     area = side * side
-    return math.ceil(area)
+    if not area.is_integer():
+        area = math.ceil(area)
+    return area
 
 
-side_length = 3
+side_length = 5.7
 area_result = square(side_length)
 
 print(f'Площадь квадрата со стороной {side_length}: {area_result}')
